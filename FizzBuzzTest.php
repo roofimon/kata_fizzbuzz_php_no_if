d3ab3b35d7cba187da3eaaad82318010d6c988ee
roofimon <roofimon@gmail.com>
2013-12-21 12:54:53 +0700

    before refactor

---
+++ FizzBuzzTest.php
@@ -0,0 +1,89 @@
+<?php
+class Fizzbuzz {
+  function __construct($initial) {
+    $this->number = $initial;
+  }
+  function toString() {
+    $str = "";
+    for ($i = 1; $i <= $this->number; $i++) {
+      $str .= $this->getElement($i);
+      $str .=", ";
+    }
+    $str = substr($str, 0, -2);
+    return $str;
+  }
+  function getElement($i) {
+    if ($this->noFizzNoBuzz($i))
+      return $i;
+    return $this->fizzBuzz($i);
+  }
+  function noFizzNoBuzz($i) {
+    return $i % 3 != 0 && $i % 5 !=0;
+  }
+  function fizzBuzz($i) {
+    $fizz = ["Fizz", "", ""];
+    $buzz = ["Buzz", "", "", "", ""];
+
+    $result = "";
+    $result .= $fizz[$i%3];
+    $result .= $buzz[$i%5];
+    return $result;
+  }
+}
+
+class FizzBuzzTest extends PHPUnit_Framework_TestCase{
+  function testgetElement1ShouldBe1() {
+    $dont_care = 0;
+    $fizzBuzz = new Fizzbuzz($dont_care);
+    $actual = $fizzBuzz->getElement(1);
+    $this->assertEquals(1, $actual);
+  }
+  function testgetElement3ShouldBeFizz() {
+    $dont_care = 0;
+    $fizzBuzz = new Fizzbuzz($dont_care);
+    $actual = $fizzBuzz->getElement(3);
+    $this->assertEquals("Fizz", $actual);
+  }
+  function testgetElement5ShouldBeBuzz() {
+    $dont_care = 0;
+    $fizzBuzz = new Fizzbuzz($dont_care);
+    $actual = $fizzBuzz->getElement(5);
+    $this->assertEquals("Buzz", $actual);
+  }
+  function testgetElement15ShouldBeFizzBuzz() {
+    $dont_care = 0;
+    $fizzBuzz = new Fizzbuzz($dont_care);
+    $actual = $fizzBuzz->getElement(15);
+    $this->assertEquals("FizzBuzz", $actual);
+  }
+  function test1ShouldReturn1() {
+    $expect = "1";
+    $fizzBuzz = new Fizzbuzz(1);
+    $actual = $fizzBuzz->toString();
+    $this->assertEquals($expect, $actual);
+  }
+  function test2ShouldReturn2() {
+    $expect = "1, 2";
+    $fizzBuzz = new Fizzbuzz(2);
+    $actual = $fizzBuzz->toString();
+    $this->assertEquals($expect, $actual);
+  }
+  function test3ShouldReturn12Fizz() {
+    $expect = "1, 2, Fizz";
+    $fizzBuzz = new Fizzbuzz(3);
+    $actual = $fizzBuzz->toString();
+    $this->assertEquals($expect, $actual);
+  }
+  function test5ShouldReturn12Fizz4Buzz() {
+    $expect = "1, 2, Fizz, 4, Buzz";
+    $fizzBuzz = new Fizzbuzz(5);
+    $actual = $fizzBuzz->toString();
+    $this->assertEquals($expect, $actual);
+  }
+  function test15ShouldReturn12Fizz4Buzz14FizzBuzz() {
+    $expect = "1, 2, Fizz, 4, Buzz, Fizz, 7, 8, Fizz, Buzz, 11, Fizz, 13, 14, FizzBuzz";
+    $fizzBuzz = new Fizzbuzz(15);
+    $actual = $fizzBuzz->toString();
+    $this->assertEquals($expect, $actual);
+  }
+}
